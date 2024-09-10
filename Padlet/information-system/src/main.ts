f23b3257b804c1ebaddfc1e015c63c6d68a4727e
XiaoDong <admin@xiaodong.moe>
2024-09-10 14:25:36 +0000

    feat(padlet): setup information-system

---
+++ Padlet/information-system/src/main.ts
@@ -0,0 +1,29 @@
+import "./style.css";
+
+import { createMemoryHistory, createRouter } from "vue-router";
+
+import AboutView from "./pages/Exercise.vue";
+import App from "./App.vue";
+import Aura from "@primevue/themes/aura";
+import HomeView from "./pages/Home.vue";
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/about", component: AboutView },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App)
+  .use(router)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  .mount("#app");
---
+++ Padlet/information-system/src/pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+.border {
+  border: 1px solid #000;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  background-color: #f0f0f0;
+}
+</style>
