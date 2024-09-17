a5d1e40f1484a5f0aeef0c40bae87d4969619a87
XiaoDong <admin@xiaodong.moe>
2024-09-17 00:11:00 +0000

    feat(quickmail): update quickmail to redirect

--- quickmail/main.js
+++ quickmail/main.js
@@ -1,8 +1,1 @@
-import "./style.css";
-
-document.querySelector("#app").innerHTML = `
-<iframe
-  id="page"
-  src="https://forms.office.com/r/6aWmNU6tz2">
-</iframe>
-`;
+window.location.href = "https://forms.office.com/r/6aWmNU6tz2";
